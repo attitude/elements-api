037fba5a62557149d7bf8c56a524a84ef5495552
Martin Adamko <martin@attitude.sk>
2013-09-27 10:40:09 +0200

    Init `Element`

---
+++ Element.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace attitude\Elements;
+
+final class API_Element
+{
+    private $request   = null;
+    private $handler   = null;
+    private $responder = null;
+
+    public function __construct(Request_Element $request)
+    {
+        $this->setRequest($request);
+
+        $handler = $this->request->getRequestURIArray();
+        $handler = array_shift($handler);
+
+        try {
+            $handler = DependencyContainer::get(get_called_class().'.handler('.$handler.')');
+            $this->setHandler($handler);
+        } catch (HTTPException $e) {
+            trigger_error($e->getMessage());
+            throw new HTTPException(400, "Cannot handle `/{$handler}`.");
+        }
+
+        // Try to load an accept
+        foreach ($this->request->getAccept() as $accept) {
+            try {
+                $responder = DependencyContainer::get(get_called_class().'.'.$accept.'_responder');
+            } catch (HTTPException $e) {
+                continue;
+            }
+
+            $this->setResponder($reponder);
+        }
+
+        if ($this->responder===null) {
+            $this->setResponder(DependencyContainer::get(get_called_class().'.responder'));
+        }
+
+        return $this;
+    }
+
+    public function setRequest(Request_Element $dependency)
+    {
+        $this->request = $dependency;
+
+        return $this;
+    }
+
+    public function setHandler(Handler_Interface $dependency)
+    {
+        $this->handler = $dependency;
+        $this->handler->setRequest($this->request);
+
+        return $this;
+    }
+
+    public function setResponder(Responder_Interface $dependency)
+    {
+        $this->responder = $dependency;
+
+        return $this;
+    }
+
+    public function respond()
+    {
+        try {
+            $data = $this->handler->handle();
+        } catch (HTTPException $e) {
+            throw $e;
+        }
+
+        $this->responder->respond($data);
+
+        return;
+    }
+}
--- README.md
+++ README.md
@@ -1,4 +1,14 @@
-elements-api
+Element: API
 ============
 
-Element: API
+###### Element: API
+
+Maybe (unnecessary) layer for handling request. Let's have it around for a while.
+It passes Request to the Handler, depending on accept it requests for the Responder.
+
+> **Hint:** Server invokes only one API per request, but API could call another API.
+
+**Respond and enjoy!**
+
+[@martin_adamko](http://twitter.com/martin_adamko)  
+*Say hi on Twitter*
